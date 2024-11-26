b42b8a0a55995b49bd46e74caaa710f19f8e74af
Julius Mboto <juliusmboto@gmail.com>
2024-11-26 20:32:08 +0300

    feat: user registration and activation

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Notifications\AccountVerificaion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Str;
 
 class UserAuthController extends Controller
